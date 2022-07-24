6805fb2db8908df811144463bf8d8eeb945a2436
Amber <enigmalea@gmail.com>
2022-07-23 20:07:49 -0400

    added comma

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Fandom-Coders', // Usually your GitHub org/user name.
   projectName: 'fandom-coders-wiki', // Usually your repo name.
-  deploymentBranch: 'gh-pages'
+  deploymentBranch: 'gh-pages',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
