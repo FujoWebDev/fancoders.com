38d3579c1985a947bfde7ba2a8236024156d8ff4
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-17 15:49:13 -0700

    Improve readability of the join us section

--- src/components/HomepageJoin/index.tsx
+++ src/components/HomepageJoin/index.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 export default function HomepageJoin(): JSX.Element {
   return (
-    <section className={styles.join}><a id="join"></a>
+    <section className={styles.join}>
+      <a id="join"></a>
       <div className="container">
         <h2>How to Join Us</h2>
-        <p>Right now Fandom Coders primarily exists as a Discord server, but we do have plans to expand to other social media. We also plan to hold public online events. For now, if you're interested in joining us, please reach out to essential-randomness on <a href="https://essential-randomness.tumblr.com">tumblr</a> or <a href="EssentialRandom">twitter</a> for an invite to the Discord.</p>
+        <p>
+          Fandom Coders primarily exists as a Discord server, with plans to
+          expand to other socials and eventually hold public online events.
+        </p>
+        <p>
+          If you're interested in joining us, reach out to essential-randomness
+          on <a href="https://essential-randomness.tumblr.com">tumblr</a> or{" "}
+          <a href="https://twitter.com/EssentialRandom">twitter</a> for a
+          Discord invite. Current members also have the ability to invite new
+          ones.
+        </p>
       </div>
     </section>
   );
