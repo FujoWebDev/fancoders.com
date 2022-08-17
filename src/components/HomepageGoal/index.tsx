9a18eaacdd5b300f7c5d8e4ca47dd255f4fa716e
Essential Randomness <essential.randomn3ss@gmail.com>
2022-08-17 15:48:26 -0700

    Add a reference to the goals writing issue

--- src/components/HomepageGoal/index.tsx
+++ src/components/HomepageGoal/index.tsx
@@ -1,13 +1,20 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 export default function HomepageGoals(): JSX.Element {
   return (
-    <section className={styles.goals}><a id="goals"></a>
+    <section className={styles.goals}>
+      <a id="goals"></a>
       <div className="container padding-bottom--lg">
         <h2>Our Goals</h2>
-        <p>Some Stuff</p>
+        <p>
+          TO WRITE (see{" "}
+          <a href="https://github.com/Fandom-Coders/fandom-coders.github.io/issues/1">
+            Issue#1
+          </a>
+          ).
+        </p>
       </div>
     </section>
   );
