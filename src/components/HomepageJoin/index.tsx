b7374dfc00ad1f43229d15db2a70b79cee481f9f
Essential Randomness <essential.randomn3ss@gmail.com>
2022-09-30 18:13:33 -0700

    add first attempt at signup form

--- src/components/HomepageJoin/index.tsx
+++ src/components/HomepageJoin/index.tsx
@@ -9,15 +9,24 @@ export default function HomepageJoin(): JSX.Element {
       <div className="container">
         <h2>How to Join Us</h2>
         <p>
-          Fandom Coders primarily exists as a Discord server, with plans to
-          expand to other socials and eventually hold public online events.
+          Fandom Coders currently exists as a Discord server, with plans to
+          expand to other socials, and eventually hold public online events. All
+          current members have the ability to invite new ones.
         </p>
         <p>
-          If you're interested in joining us, reach out to essential-randomness
-          on <a href="https://essential-randomness.tumblr.com">tumblr</a> or{" "}
+          If you're interested in joining us, you can sign up through this form:
+          <form name="fandom-coders-signup" method="POST" data-netlify="true">
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+            <button type="submit">Send</button>
+          </form>
+        </p>
+        <p>
+          Alternatively, reach out to Ms Boba on{" "}
+          <a href="https://essential-randomness.tumblr.com">tumblr</a> or{" "}
           <a href="https://twitter.com/EssentialRandom">twitter</a> for a
-          Discord invite. Current members also have the ability to invite new
-          ones.
+          Discord invite.
         </p>
       </div>
     </section>
