be7ce6100e4d07b1035402e43e2f4f9f16b844df
Essential Randomness <essential.randomn3ss@gmail.com>
2023-07-05 18:06:03 -0700

    add styles to personal site card

---
+++ src/components/PersonalSiteCard/index.tsx
@@ -0,0 +1,24 @@
+import Link from "@docusaurus/Link";
+import React from "react";
+import styles from "./styles.module.css";
+
+type PersonalSiteCardProps = {
+  name: string;
+  url: string;
+  creator: string;
+  description: string;
+  tools: string;
+  host: string;
+  registrar: string;
+};
+
+function PersonalSiteCard(props: PersonalSiteCardProps) {
+  const { name, url, creator, description, tools, host, registrar } = props;
+  return (
+    <div className={styles.siteCard}>
+      <a href={url}>{name}</a> by {creator}
+    </div>
+  );
+}
+
+export default PersonalSiteCard;
